2635e003d9c72aa3079fcb2c7dcbe2652d9a29f9
iuri silva <iuriksilv33@gmail.com>
2019-06-27 15:25:34 -0300

    Substituir caractere invalido
    Os inputs numericos nao aceitavam nenhum tipo de caractere, mas quando o
    usuario digitava um numero com virgula ou com ponto ele considerava como
    um caractere invalido,entao foi feita uma funcao pra tratar este erro.

--- calc.go
+++ calc.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	var primeiroDigito , segundoDigito string
-	var operador string
-	var resultado int
+	var primeiroDigito, operador, segundoDigito string
+	var resultado float64
 
 	fmt.Print("digite um numero:")
 	fmt.Scan(&primeiroDigito)
@@ -19,15 +19,15 @@ func main() {
 	fmt.Print("digite outro numero:")
 	fmt.Scan(&segundoDigito)
 
-	primeiroValor, err := strconv.Atoi(primeiroDigito)
+	primeiroValor, err := tratarValor(primeiroDigito)
 	if err != nil {
-		fmt.Println("Erro !", err)
+		fmt.Println("Primeiro digito invalido")
 		return
 	}
 
-	segundoValor, err := strconv.Atoi(segundoDigito)
+	segundoValor, err := tratarValor(segundoDigito)
 	if err != nil {
-		fmt.Println("Erro !", err)
+		fmt.Println("Segundo digito invalido")
 		return
 	}
 
@@ -45,5 +45,14 @@ func main() {
 		return
 	}
 
-	fmt.Println(primeiroValor,operador,segundoValor,"=",resultado)
+	fmt.Println(primeiroValor, operador, segundoValor, "=", resultado)
+}
+func tratarValor(valorDigitado string) (float64, error) {
+	valorDigitado = strings.Replace(valorDigitado, ",", ".", -1)
+	valorTratado, err := strconv.ParseFloat(valorDigitado, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return valorTratado, err
 }
