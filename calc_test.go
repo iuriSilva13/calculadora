49d48a1bb859c63100c33eda21a6a559efe3d49a
iuri silva <iuriksilv33@gmail.com>
2019-08-20 13:43:56 -0300

    Testar funcao tratarValor com inteiro
    Foi testado a funcao tratarValor com numero inteiro, a funcao espera
    receber um float64.

--- calc_test.go
+++ calc_test.go
@@ -53,3 +53,38 @@ func Test_calcularValores(teste *testing.T) {
                 })
         }
 }
+func Test_tratarValor(teste *testing.T) {
+        type parâmetrosRecebidos struct {
+                valorDigitado string
+                digito        string
+        }
+        testes := []struct {
+                mensagemDeIdentificação string
+                parâmetrosRecebidos func(teste *testing.T) parâmetrosRecebidos
+
+                valorEsperado float64
+        }{
+                {
+                        mensagemDeIdentificação: "Inteiro deve ser identificado corretamente",
+                        parâmetrosRecebidos: func(*testing.T) parâmetrosRecebidos {
+                                return parâmetrosRecebidos{
+                                        valorDigitado: "10",
+                                        digito:        "primeiro",
+                                }
+                        },
+                        valorEsperado: 10.0,
+                },
+        }
+
+        for _, valorTeste := range testes {
+                teste.Run(valorTeste.mensagemDeIdentificação, func(teste *testing.T) {
+                        testeTratarValor := valorTeste.parâmetrosRecebidos(teste)
+
+                        valorRecebido := tratarValor(testeTratarValor.valorDigitado, testeTratarValor.digito)
+
+                        if !reflect.DeepEqual(valorRecebido, valorTeste.valorEsperado) {
+                                teste.Errorf("tratarValor valorRecebido = %v, valorEsperado: %v", valorRecebido, valorTeste.valorEsperado)
+                        }
+                })
+        }
+}
