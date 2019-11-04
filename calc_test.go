12013760311e6e56de269aeced0bc9b7fb2d9d87
iuri silva <iuriksilv33@gmail.com>
2019-11-04 10:12:44 -0300

    Testar funcao com p/ valor e operador em branco
    A funcao calculadoraWeb foi testada com o parametro do primeiro valor em branco,
    operador em branco, segundo valor preenchido e status 417.

--- calc_test.go
+++ calc_test.go
@@ -174,6 +174,12 @@ func Test_calculadoraWeb(t *testing.T) {
 			w:                       httptest.NewRecorder(),
 			status:                  http.StatusExpectationFailed,
 		},
+		{
+			mensagemDeIdentificação: "Parâmetros em branco do primeiro valor e operador devem ser identificados corretamente",
+			url:                     "http://localhost:8080/calculadora?v1=&operador=&v2=6",
+			w:                       httptest.NewRecorder(),
+			status:                  http.StatusExpectationFailed,
+		},
 	}
 
 	for _, valorTeste := range testes {
